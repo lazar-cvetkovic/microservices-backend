d57c29cd4eaf4636a4809a1fdc400e4767e6b6f2
Lazar Cvetkovic <laki.cvetkovic@gmail.com>
2025-09-27 12:52:13 +0200

    Flyway deployment fixes

--- player-service/src/main/java/com/artemi/player/application/PlayerServiceApplication.java
+++ player-service/src/main/java/com/artemi/player/application/PlayerServiceApplication.java
@@ -1,9 +1,15 @@
 package com.artemi.player.application;
 
+import com.artemi.player.infrastructure.persistence.JpaPlayerEntity;
+import com.artemi.player.infrastructure.persistence.SpringDataPlayerJpaRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "com.artemi.player")
+@SpringBootApplication
+@EnableJpaRepositories(basePackageClasses = SpringDataPlayerJpaRepository.class)
+@EntityScan(basePackageClasses = JpaPlayerEntity.class)
 public class PlayerServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PlayerServiceApplication.class, args);
--- player-service/src/main/java/com/artemi/player/infrastructure/persistence/SpringDataPlayerJpaRepository.java
+++ player-service/src/main/java/com/artemi/player/infrastructure/persistence/SpringDataPlayerJpaRepository.java
@@ -1,9 +1,11 @@
 package com.artemi.player.infrastructure.persistence;
 
 import org.springframework.data.jpa.repository.*;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public interface SpringDataPlayerJpaRepository extends JpaRepository<JpaPlayerEntity, String> {
     Optional<JpaPlayerEntity> findByUsernameIgnoreCase(String username);
 
